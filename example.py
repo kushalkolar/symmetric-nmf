c50c96e5642aa315cf05a50f914daa01353782d3
kushalkolar <kushalkolar@gmail.com>
2024-12-02 18:01:16 -0500

    cleanup, add example

---
+++ example.py
@@ -0,0 +1,37 @@
+import numpy as np
+from symmnmf import SymNMF
+from matplotlib import pyplot as plt
+
+
+np.random.seed(0)
+
+A = np.array([
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1],
+]).astype(np.float32)
+
+A += np.abs(np.random.normal(scale=0.3, size=A.size)).reshape(A.shape)
+
+snmf = SymNMF(k=3)
+snmf.fit(A)
+
+fig, axs = plt.subplots(1, 2, figsize=(7, 4))
+
+axs[0].imshow(A)
+axs[0].set_title("Affinity matrix")
+
+axs[1].imshow(snmf.H)
+axs[1].set_title("Cluster memberships")
+axs[1].set_xlabel("cluster")
+axs[1].set_ylabel("node")
+
+fig.tight_layout()
+fig.show()
+
--- symmnmf/anls.py
+++ symmnmf/anls.py
@@ -5,46 +5,46 @@ from .nnls import nnlsm_blockpivot
 
 
 class SymNMF:
+    """Symmetric NMF using ANLS"""
     def __init__(
             self,
-            A: np.ndarray,
             k: int,
-            H: np.ndarray = None,
             maxiter: int = 10_000,
             tol: float = 10e-3,
             alpha: float = None,
     ):
+
+        self.alpha_ = alpha
+        self.maxiter_ = maxiter
+        self.tol_ = tol
+        self.k_ = k
+
+    def fit(self, A: np.ndarray, H_init: np.ndarray = None):
         self.A = A
 
-        if H is None:
-            self.H = 2 * np.sqrt(np.mean(np.mean(A) / k)) * np.random.rand(self.A.shape[0], k)
-
+        if H_init is None:
+            H_init = 2 * np.sqrt(np.mean(np.mean(A) / self.k_)) * np.random.rand(self.A.shape[0], self.k_)
         else:
-            self.H = H
+            if H_init.shape[0] != A.shape[0]:
+                raise ValueError(f"H_init.shape[0] != A.shape[0], {H_init.shape[0]} != {A.shape[0]}")
 
-        if self.H.shape[0] != A.shape[0]:
-            raise ValueError
+        self.H_init_ = H_init
 
-        if alpha is None:
-            self.alpha = np.max(A) ** 2
-        else:
-            self.alpha = alpha
+        if self.alpha_ is None:
+            self.alpha_ = np.max(self.A) **2
 
-        self.maxiter = maxiter
-        self.tol = tol
-        self.k = k
+        self.H = self.H_init_.copy()
 
-    def fit(self):
         self.W = self.H
-        self.I_k = self.alpha * np.eye(self.k)
+        I_k = self.alpha_ * np.eye(self.k_)
 
         self.left = self.H.conj().T @ self.H
         self.right = self.A @ self.H
 
-        for self.i in tqdm(range(self.maxiter)):
+        for i in tqdm(range(self.maxiter_)):
             self.W = nnlsm_blockpivot(
-                self.left + self.I_k,
-                (self.right + self.alpha * self.H).conj().T,
+                self.left + I_k,
+                (self.right + self.alpha_ * self.H).conj().T,
                 True,
                 self.W.conj().T
             )[0].T
@@ -53,15 +53,15 @@ class SymNMF:
             self.right = self.A @ self.W
 
             self.H = nnlsm_blockpivot(
-                self.left + self.I_k,
-                (self.right + self.alpha * self.W).conj().T,
+                self.left + I_k,
+                (self.right + self.alpha_ * self.W).conj().T,
                 True,
                 self.H.conj().T
             )[0].T
             # tempW = W.sum(axis=1)
             # tempH = H.sum(axis=1)
 
-            temp = self.alpha * (self.H - self.W)
+            temp = self.alpha_ * (self.H - self.W)
 
             gradH = self.H @ self.left - self.right + temp
             self.left = self.H.conj().T @ self.H
@@ -72,9 +72,9 @@ class SymNMF:
             W_norm = np.linalg.norm(gradW[(gradW <= 0) | (self.W > 0)])
             H_norm = np.linalg.norm(gradH[(gradH <= 0) | (self.H > 0)])
 
-            if self.i == 0:
+            if i == 0:
                 initgrad = np.sqrt(W_norm ** 2 + H_norm ** 2)
             else:
                 projnorm = np.sqrt(W_norm ** 2 + H_norm ** 2)
-                if projnorm < self.tol * initgrad:
+                if projnorm < self.tol_ * initgrad:
                     break
